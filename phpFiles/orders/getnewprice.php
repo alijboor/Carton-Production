48c2c52358cd5881bee94938e834de54c9890d6e
alijboor <alijboor.1992@gmail.com>
2022-07-20 10:29:20 +0300

    final changes

--- phpFiles/orders/getnewprice.php
+++ phpFiles/orders/getnewprice.php
@@ -4,9 +4,9 @@ session_start();
 require '../connection.php';
 $arrlist = [];
 
-$res_orderdet=mysqli_query($conn, "SELECT * FROM items");
+$res_orderdet=mysqli_query($conn, "SELECT * FROM item_last_price");
 while($res= mysqli_fetch_assoc($res_orderdet)){
-    array_push($arrlist, array($res["itemid"],$res["type"],abs($res["price"])));
+    array_push($arrlist, array($res["userid"],$res["itemid"],abs($res["new_price"])));
 }
 echo json_encode($arrlist);
 ?>
--- phpFiles/orders/gitorder.php
+++ phpFiles/orders/gitorder.php
@@ -4,9 +4,9 @@ session_start();
 require '../connection.php';
 $arrlist = [];
 
-$res_orderdet=mysqli_query($conn, "SELECT * FROM orders ORDER BY orderdate DESC");
+$res_orderdet=mysqli_query($conn, "SELECT o.*, u.username FROM orders o JOIN useraccount u ON (o.userid = u.id) ORDER BY orderdate DESC");
 while($res= mysqli_fetch_assoc($res_orderdet)){
-    array_push($arrlist, array($res["orderid"],$res["userid"],$res["custName"],$res["note1"],$res["note2"],$res["status"],$res["itemsdetail"],$res["orderdate"]));
+    array_push($arrlist, array($res["orderid"],$res["userid"],$res["custName"],$res["note1"],$res["note2"],$res["status"],$res["itemsdetail"],$res["orderdate"],$res["username"]));
 }
 echo json_encode($arrlist);
 ?>
